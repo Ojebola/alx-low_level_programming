6bd7d1f9dfc3605807f6591ee003f64e1a9aa61b
Oluwatimilehin Ojediran <ojediran.oluwatimilehin@gmail.com>
2022-06-06 07:56:57 -0700

    Task 0

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,15 @@
  */
 int main(void)
 {
-printf("Size of a char: %d byte(s)\n", sizeof(char));
-printf("Size of an int: %d byte(s)\n", sizeof(int));
-printf("Size of a long: %d byte(s)\n", sizeof(long int));
-printf("Size of a long long: %d byte(s)\n", sizeof(long long int));
-printf("Size of a float: %d byte(s)\n", sizeof(float));
+char = a;
+int = b;
+long = c;
+long long int = d;
+float = e;
+printf("Size of a char: %d byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of an int: %d byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long int: %d byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a long long int: %d byte(s)\n", (unsigned long)sizeof(d);
+printf("Size of a float: %d byte(s)\n", (unsigned long)sizeof(e));
 return (0);
 }
---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/* more headers goes there */
+/**
+ * main - Prints out a string to stdout.
+ * Description: this prins out string
+ * Return: 0 if success.
+ */
+
+
+/* betty style doc for function main goes there */
+
+int main(void)
+{
+	int n;
+
+
+
+	srand(time(0));
+
+	n = rand() - RAND_MAX / 2;
+
+	/* your code goes there */
+	if (n > 0)
+	{
+		printf(%d, "is positive\n");
+	}
+	else if (n < 0)
+	{
+		printf(%d, "is negative\n");
+	}
+	else
+	{
+		printf(%d, "is zero\n");
+
+	return (0);
+
+}
