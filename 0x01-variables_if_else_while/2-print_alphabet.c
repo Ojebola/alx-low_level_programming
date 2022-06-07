2b31db92b7d34c38c1dbba3b015216ef127a672d
Oluwatimilehin Ojediran <ojediran.oluwatimilehin@gmail.com>
2022-06-06 20:34:47 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints out alphabet in lowercase
+ * Description: this prins out string
+ * Return: 0 if success.
+ */
+int main(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
