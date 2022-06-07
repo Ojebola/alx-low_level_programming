7d05732d6aca94696a814178bebf8476134d523f
Oluwatimilehin Ojediran <ojediran.oluwatimilehin@gmail.com>
2022-06-06 21:01:38 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Prints out alphabet in lowercase except for e and q
+ * Description: this prins out string
+ * Return: 0 if success.
+ */
+int main(void)
+{
+	char ch = 'a';
+	while (ch <= 'z');
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
