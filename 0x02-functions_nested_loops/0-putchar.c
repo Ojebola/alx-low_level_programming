f93d74cdfaaedd314e8e02b9109da8297a941271
Oluwatimilehin Ojediran <ojediran.oluwatimilehin@gmail.com>
2022-06-08 05:12:25 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - a program that prints _putchar
+ * Description: this code prints out using putchar
+ * Return: 0 if success
+ */
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}
