937b13579f61ee1ee72ce1830ff5b380fab0fbc1
Oluwatimilehin Ojediran <ojediran.oluwatimilehin@gmail.com>
2022-06-14 21:00:20 -0700

    Task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 /**
- * reet_to_98 - takes a pointer to an int parameter updates the value and points to
- * @n - int parameter
- * Return - Nothing
+ * reset_to_98 - takes a pointer to an int parameter updates the value to
+ * @n- int parameter
+ * Return- Nothing
  */
 
 void reset_to_98(int *n)
